547af39c1463f440355f1d39b8cc048eb1ada6b2
Saharshv <thesaharsh@gmail.com>
2019-04-13 15:21:53 -0400

    Finished Scrape

---
+++ server/scrape.py
@@ -0,0 +1,53 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+import sys
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+
+    except RequestException as e:
+        log_error('Error during requests to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 
+            and content_type is not None 
+            and content_type.find('html') > -1)
+
+
+def log_error(e):
+    """
+    It is always a good idea to log errors. 
+    This function just prints them, but you can
+    make it do anything.
+    """
+    print(e)
+	
+def scrape():
+	result_list = []
+	raw_html = simple_get('https://sentence.yourdictionary.com/'+sys.argv[1])
+	html = BeautifulSoup(raw_html, 'html.parser')
+	result_block = html.find_all('div', attrs={'class':'li_content'})
+	for result in result_block:
+		result_list.append(result.get_text())
+	return(result_list)
+
+if __name__=='__main__':
+	print(scrape())
