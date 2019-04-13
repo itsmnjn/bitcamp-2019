5887fa27234483105e1a6377396d2922de57855a
devericx <devericx@gmail.com>
2019-04-13 19:52:41 -0400

    Add basic Chrome extension functionality

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,23 +1,54 @@
+var root_url = "http://localhost:5000/";
+var selection = "";
+
 function contextly(info) {
-    var word = info.selectionText;
-    chrome.tabs.create({ url: "https://google.com/search?q=" + word });
+    chrome.storage.sync.get(["nat_lang"], function(result) {
+        var word = info.selectionText;
+        chrome.tabs.create({
+            url: root_url + word + "?nat_lang=" + result.nat_lang
+        });
+    });
+}
+
+function getSelected() {
+    var userSelection;
+    if (window.getSelection) {
+        userSelection = window.getSelection();
+    } else if (document.selection) {
+        userSelection = document.selection.createRange();
+    }
+    return userSelection;
+}
+
+function getSelectedNode() {
+    if (document.selection)
+        return document.selection.createRange().parentElement();
+    else {
+        var selection = window.getSelection();
+        if (selection.rangeCount > 0)
+            return selection.getRangeAt(0).startContainer.parentNode;
+    }
+}
+
+function storeSelection() {
+    return [window.getSelection()];
 }
 
 var rule = {
     conditions: [
         new chrome.declarativeContent.PageStateMatcher({
-            css: ["p"]
+            css: ["body"]
         })
     ],
     actions: [new chrome.declarativeContent.ShowPageAction()]
 };
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({ nat_lang: "kor" }, function() {
-        console.log("Native language: kor");
-    });
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([rule]);
+    });
+    chrome.storage.sync.set({ nat_lang: "ko" }, function(result) {
+        console.log("nat_lang: " + "ko");
     });
     chrome.contextMenus.create({
         title: "Contextly",
@@ -26,4 +57,13 @@ chrome.runtime.onInstalled.addListener(function() {
     });
 });
 
-chrome.contextMenus.onClicked.addListener(contextly);
+document.addEventListener("mouseup", storeSelection, false);
+
+chrome.contextMenus.onClicked.addListener(function(info) {
+    var selection = getSelected() + "";
+    var parentDiv = getSelectedNode();
+    console.log(document);
+    var index = parentDiv.indexOf(selection);
+    console.log(parentDiv.substr(0, index));
+    contextly(info);
+});
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,3 +1,20 @@
+function set_lang(lang, list) {
+    chrome.storage.sync.set({ nat_lang: lang }, function(result) {
+        list.value = lang;
+    });
+}
+
+var nat_lang_list = document.getElementById("nat_lang_list");
 chrome.storage.sync.get(["nat_lang"], function(result) {
-    console.log("Value currently is " + result.key);
+    nat_lang_list.value = result.nat_lang;
 });
+
+nat_lang_list.addEventListener(
+    "change",
+    function() {
+        nat_lang = nat_lang_list.options[nat_lang_list.selectedIndex].value;
+        console.log(nat_lang);
+        set_lang(nat_lang, nat_lang_list);
+    },
+    false
+);
